090b30c859dab3d6499bff1e9d69a411ff495fd7
Luiz Olivetti <luizolivetti@bap.com.br>
2022-06-17 17:26:02 -0300

    algoritmos como esperado no curso

---
+++ c/mario1-2.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+//main program
+int main(void)
+{
+     // global
+     int height;
+
+     // whats size?
+     do
+     {
+          height = get_int("Height: ");
+     }
+     while (height<=0);
+
+     // loop
+     for(int row=1; row<=height; row++)
+     {
+         for(int col=1; col<=height-row; col++){
+               printf("%s", " ");
+         }
+         for(int col=1; col<=row; col++){
+               printf("%s", "#");
+         }
+         printf("%s\n", "");
+     }
+}
